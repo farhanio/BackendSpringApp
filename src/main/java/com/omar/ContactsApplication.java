a155ac35444b43f0e536cf7d1fb6179428323eee
farhanio <omar.farhani@outlook.com>
2018-03-01 11:56:26 +0100

    Backend Spring Application - RestAPI - Angular4

---
+++ src/main/java/com/omar/ContactsApplication.java
@@ -0,0 +1,41 @@
+package com.omar;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.omar.dao.ContactRepository;
+import com.omar.entities.Contact;
+
+@SpringBootApplication
+public class ContactsApplication implements CommandLineRunner{
+
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		contactRepository.save(new Contact("Omar", "Farhani", new Date() , "omar@farhani.com", 	50734889, ""));
+		
+		contactRepository.save(new Contact("Sonia", "Chikhoui", df.parse("01/01/1999") , "omalo@farhani.com", 	50734889, ""));
+		
+		contactRepository.save(new Contact("Elite", "Book", df.parse("01/01/2015") , "farha@farhani.com", 	12345678, ""));
+		
+		contactRepository.findAll().forEach(c->{
+			
+			System.out.println(c.getNom()+"  "+c.getPrenom()+"  "+c.getDateNaissance());
+
+		});
+	
+	}
+}
---
+++ src/main/java/com/omar/web/ContactWebService.java
@@ -0,0 +1,61 @@
+package com.omar.web;
+
+import java.util.List;
+
+import javax.ws.rs.QueryParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.omar.dao.ContactRepository;
+import com.omar.entities.Contact;
+
+@RestController
+public class ContactWebService {
+
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@RequestMapping(value="/contacts", method=RequestMethod.GET)
+	public List<Contact> getContacts(){
+		return contactRepository.findAll();
+	}
+	
+	@RequestMapping(value="/contacts/{id}", method=RequestMethod.GET)
+	public Contact getContact(@PathVariable Long id){
+		return contactRepository.findOne(id);
+	}
+	
+	@RequestMapping(value="/contacts", method=RequestMethod.POST)
+	public Contact saveContact(@RequestBody Contact contact){
+		return contactRepository.save(contact);
+	}
+	
+	@RequestMapping(value = "/contacts/{id}", method = RequestMethod.DELETE)
+	public void deleteContact(@PathVariable Long id) {
+		contactRepository.delete(id);
+	}
+	
+	@RequestMapping(value="/contacts", method=RequestMethod.PUT)
+	public Contact updateContact(@PathVariable Long id, @RequestBody Contact contact){
+		contact.setId(id);
+		return contactRepository. save(contact);
+	}
+	
+	@RequestMapping(value="/chercherContacts", method=RequestMethod.GET)
+	public Page<Contact> chercher(@RequestParam(name="mc", defaultValue= "") String mc,
+			@RequestParam(name="page", defaultValue= "0") int page,
+			@RequestParam(name="size", defaultValue= "1") int size){
+		return contactRepository.chercher("%"+mc+"%", new PageRequest(page, size)) ;
+	}
+	
+}
